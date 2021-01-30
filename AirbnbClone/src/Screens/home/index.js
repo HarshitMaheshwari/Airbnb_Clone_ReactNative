1d549b446eba89619bf2753072e27c93e09687c8
HarshitMaheshwari <mr.harshit1997@gmail.com>
2021-01-30 18:16:26 +0000

    Home screen search button

--- AirbnbClone/src/Screens/home/index.js
+++ AirbnbClone/src/Screens/home/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {View, Text, ImageBackground, Pressable} from 'react-native';
 import styles from './styles';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const HomeScreen = (props) => {
     return(
-        <View> 
+        <View>
+            <Pressable style={styles.searchButton} onPress={() => console.warn('Search Button Clicked')}>
+                <Fontisto name="search" size={25} color={"#f15454"}/>
+                <Text style={styles.buttonText}>Where are you going?</Text>
+            </Pressable> 
 
             <ImageBackground 
                 source={require('../../../assets/images/wallpaper.jpg')} style={styles.image}>
                 <Text style = {styles.title}>Go near</Text>
                 <Pressable style={styles.button} onPress={() => console.warn('Explore Button Clicked')}>
-                    <Text>Explore NearBy Stays!</Text>
+                    <Text style={styles.buttonText}>Explore NearBy Stays!</Text>
                 </Pressable>
             </ImageBackground>
 
--- AirbnbClone/src/Screens/home/styles.js
+++ AirbnbClone/src/Screens/home/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create ({
     image: {
@@ -14,6 +14,41 @@ const styles = StyleSheet.create ({
         color: 'white',
         width: '70%',
         marginLeft: 25,
+    },
+
+    button: {
+        backgroundColor: 'white',
+        width: 200,
+        marginLeft: 25,
+        marginTop: 25,
+        height: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    searchButton: {
+        backgroundColor: 'white',
+        position: 'absolute',
+        width: Dimensions.get('screen').width - 20,
+        top: 20,
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 50,
+    },
+
+    searchButtonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
     }
 
 });
